d9dcb55b63ebebe38ffcaa04507508316c77b169
Mukka Deepak <ramu92013@gmail.com>
2021-06-25 15:56:41 +0530

    📦 added tailwind.css package

---
+++ src/Components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="bg-blue-900 h-14 flex justify-around items-center w-screen p-2">
+            <h2 className="font-semibold text-2xl text-gray-200">Hello from Navbar</h2>
+            <h2 className="font-semibold text-2xl text-gray-200">Hello from Navbar</h2>
+            <h2 className="font-semibold text-2xl text-gray-200">Hello from Navbar</h2>
+        </div>
+    )
+}
+
+export default Navbar
