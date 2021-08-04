c1ba572c40992a245fe1357c15c674dfd6372236
Mukka Deepak <ramu92013@gmail.com>
2021-08-04 15:53:54 +0530

    👷 added page title and icon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Suggestions from "./Components/Suggestions";
 
 function App() {
   return (
+    <div>
+      <title>NewsRead</title>
+      <meta
+      name="description"
+      content="Update yourselves with trending news"
+    />
     <div className="bg-blue-100 md:flex md:flex-col flex-wrap h-full w-full text-center items-center dark:bg-gray-800">
       <Navbar />
       <div className="md:flex md:flex-row w-full justify-evenly md:h-5/6 flex-wrap">
@@ -11,6 +17,7 @@ function App() {
         <Suggestions />
       </div>
       <h4 className="text-base pb-4 dark:text-white">Developed by 👨‍💻 Mukka Deepak </h4>
+    </div>
     </div>
   );
 }
